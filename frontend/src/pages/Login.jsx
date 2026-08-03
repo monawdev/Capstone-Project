95f8080e7a5b447d4747126631ee58515b073363
منور <125405236+monawdev@users.noreply.github.com>
2026-08-03 13:57:10 -0400

    Create login and registration forms

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,74 @@
+import { useState } from "react";
+import "../styles/Auth.css";
+
+
 function Login() {
-  return <h1>Login</h1>;
+
+  const [formData, setFormData] = useState({
+    email:"",
+    password:""
+  });
+
+
+  const handleChange = (e)=>{
+
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+
+  };
+
+
+  const handleSubmit=(e)=>{
+
+    e.preventDefault();
+
+    console.log(formData);
+
+  };
+
+
+  return (
+
+    <div className="auth-container">
+
+      <h1>Login</h1>
+
+
+      <form onSubmit={handleSubmit}>
+
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+
+        <button type="submit">
+          Login
+        </button>
+
+
+      </form>
+
+
+    </div>
+
+  );
 }
+
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,72 @@
+import { useState } from "react";
+import "../styles/Auth.css";
+
 function Register() {
-  return <h1>Register</h1>;
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
+
+
+  return (
+    <div className="auth-container">
+
+      <h1>Create Account</h1>
+
+      <form onSubmit={handleSubmit}>
+
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+
+        <button type="submit">
+          Register
+        </button>
+
+      </form>
+
+    </div>
+  );
 }
 
 export default Register;
