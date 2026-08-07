9cf68702fb5e4cfa96efcaeead93612805c47bba
منور <125405236+monawdev@users.noreply.github.com>
2026-08-07 12:40:48 -0400

    Improve navbar and global styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,20 +2,52 @@ import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 
 function Navbar() {
-  return (
-    <nav>
-      <h2>Car Marketplace</h2>
 
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/cars">Cars</Link>
-        <Link to="/add-car">Add Car</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/profile">Profile</Link>
+  return (
+
+    <nav className="navbar">
+
+
+      <Link to="/" className="logo">
+        Car Marketplace
+      </Link>
+
+
+      <div className="nav-links">
+
+        <Link to="/">
+          Home
+        </Link>
+
+
+        <Link to="/cars">
+          Cars
+        </Link>
+
+
+        <Link to="/add-car">
+          Add Car
+        </Link>
+
+
+        <Link to="/login">
+          Login
+        </Link>
+
+
+        <Link to="/register">
+          Register
+        </Link>
+
+
       </div>
+
+
     </nav>
+
   );
+
 }
+
 
 export default Navbar;
